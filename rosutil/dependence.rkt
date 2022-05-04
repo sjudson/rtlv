10570b619b50b2cf56bd423b7455f6d0e17f9fec
jeremy-weiss <j.weiss@yale.edu>
2022-05-04 15:59:17 -0400

    verify all states and print

--- rosutil/dependence.rkt
+++ rosutil/dependence.rkt
@@ -25,15 +25,28 @@
   (define value-allowed-symbolics (set-intersect value-symbolics constants))
   ; not okay to depend on these:
   (define value-rest-symbolics (set-subtract value-symbolics constants))
+  ;;; (@define-symbolic* freshh (@type-of value))
+  ;;; (define res
+  ;;;   (@synthesize
+  ;;;     #:forall (set->list value-rest-symbolics)
+  ;;;     #:guarantee
+  ;;;       (@assert (@equal? value freshh))
+  ;;;   ))
+  ;;; (printf "synth: ~a\n" res)
+  ;;; (@clear-vc!)
+
   (cond
     [(set-empty? value-rest-symbolics)
      ; fast path
+    ;;;  (printf "rest-empty\n")
      (@unsat)]
     [(set-empty? value-allowed-symbolics)
      ; fast-ish path when we're trying to show that something
      ; is concrete (no dependence on any constants) --
      ; no exists/forall style query required
-     (concrete value)]
+     (define concrete-value (concrete value))
+     (printf "allowed-empty: ~a\t~a\n" value (@evaluate value concrete-value))
+     concrete-value]
     [else
      ; need to invoke solver
      ; try to show that value doesn't depend on other symbolics
